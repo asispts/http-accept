9fa8dedb924230057087655b50efc178db30bcb4
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-04-27 19:21:09 +0900

    Add Accept-Encoding parser

---
+++ src/AcceptEncodingParser.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace HttpAccept;
+
+use HttpAccept\Data\MediaType;
+use HttpAccept\Utility\Parser;
+use HttpAccept\Utility\QValueSorter;
+
+final class AcceptEncodingParser
+{
+    private $parser;
+
+    public function __construct()
+    {
+        $this->parser = new Parser(null, new QValueSorter());
+    }
+
+    /**
+     * @return MediaType[]
+     */
+    public function parse(string $source): array
+    {
+        return $this->parser->parse($source);
+    }
+}
---
+++ tests/AcceptEncodingParserTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace HttpAccept\Tests;
+
+use Generator;
+use HttpAccept\AcceptEncodingParser;
+use HttpAccept\Data\MediaType;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+
+final class AcceptEncodingParserTest extends TestCase
+{
+    /**
+     * @dataProvider invalidDataProvider
+     *
+     * @param class-string<Throwable> $exception
+     */
+    public function test_parse_invalid_data(string $source, string $exception, string $message): void
+    {
+        $this->expectException($exception);
+        $this->expectExceptionMessage($message);
+
+        (new AcceptEncodingParser())->parse($source);
+    }
+
+    public static function invalidDataProvider(): Generator
+    {
+        yield['', InvalidArgumentException::class, 'Media name is empty'];
+    }
+
+    /**
+     * @dataProvider validDataProvider
+     *
+     * @param MediaType[] $expected
+     */
+    public function test_valid_data(string $source, array $expected): void
+    {
+        $objs = (new AcceptEncodingParser())->parse($source);
+        $this->assertEquals($expected, $objs);
+    }
+
+    public function validDataProvider(): Generator
+    {
+        yield[
+          '*;q=0, identity;q=0.5, gzip;q=1.0',
+          [
+            new MediaType('gzip', [], 1000.0),
+            new MediaType('identity', ['q' => '0.5'], 500.0),
+            new MediaType('*', ['q' => '0'], 0.0),
+          ],
+        ];
+    }
+}
